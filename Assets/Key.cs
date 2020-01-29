695aef91776f7fda22bef26bf046c525ac22ce04
Siddhant Rathor <cdsid10@gmail.com>
2020-01-29 17:53:12 +0000

    Implemented Key and Door System

---
+++ Assets/Key.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] private KeyType keyType;
+    public enum KeyType
+    {
+        Red,
+        Green,
+        Blue
+    }
+
+    public KeyType GetKeyType()
+    {
+        return keyType;
+    }
+
+    
+}
