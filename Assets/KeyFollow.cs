3ccb7c43b0fd7c52edc79d60b28a278f1c8d3234
Siddhant Rathor <cdsid10@gmail.com>
2020-02-03 19:18:52 +0000

    Implemented a key follow system

---
+++ Assets/KeyFollow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyFollow : MonoBehaviour
+{
+    public static SpringJoint2D spring;
+    public bool hasKey = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spring = GetComponent<SpringJoint2D>();
+        spring.enabled = false;
+        GameObject backpack = GameObject.FindWithTag("Backpack");
+        spring.connectedBody = backpack.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            spring.enabled = true;
+            hasKey = true;
+        }
+    }
+
+
+}
