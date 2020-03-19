d3f1f4e236c5cf83f5621ef3d0d82017e476d2f5
Siddhant Rathor <cdsid10@gmail.com>
2020-03-19 16:58:24 +0000

    Implemented Boss Scene(Early Test #2)

--- Assets/BossController.cs
+++ Assets/BossController.cs
@@ -6,6 +6,19 @@ public class BossController : MonoBehaviour
 {
     public static BossController instance;
 
+    public BossAction[] actions;
+    private int currentAction;
+    private float actionCounter;
+
+    private float shotCounter;
+    private Vector2 moveDir;
+    public Rigidbody2D theRB;
+
+    public int currentHealth;
+
+    public GameObject deathEffect, hitEffect;
+    public GameObject levelExit;
+
 
     private void Awake()
     {
@@ -15,12 +28,98 @@ public class BossController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        actionCounter = actions[currentAction].actionLength;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     if(actionCounter > 0)
+        {
+            actionCounter -= Time.deltaTime;
+
+            //handle movement
+            moveDir = Vector2.zero;
+            if(actions[currentAction].shouldMove)
+            {
+                if(actions[currentAction].shouldChasePlayer)
+                {
+                    moveDir = PlayerMovement.instance.transform.position - transform.position;
+                    moveDir.Normalize();
+                }
+
+                if(actions[currentAction].moveToPoint)
+                {
+                    moveDir = actions[currentAction].pointToMoveTo.position - transform.position;
+                }
+            }
+
+
+            theRB.velocity = moveDir * actions[currentAction].moveSpeed;
+
+            //handle shooting
+            if(actions[currentAction].shouldShoot)
+            {
+                shotCounter -= Time.deltaTime;
+                if(shotCounter <= 0)
+                {
+                    shotCounter = actions[currentAction].timeBwShots;
+
+                    foreach(Transform t in actions[currentAction].shotPoints)
+                    {
+                        Instantiate(actions[currentAction].itemToShoot, t.position, t.rotation);
+                    }
+                }
+            }
+        }
+     else
+        {
+            currentAction++;
+            if(currentAction >= actions.Length)
+                {
+                currentAction = 0;
+                }
+            actionCounter = actions[currentAction].actionLength;
+        }
     }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        if(currentHealth <= 0)
+        {
+            gameObject.SetActive(false);
+
+            Instantiate(deathEffect, transform.position, transform.rotation);
+
+            if (Vector3.Distance(PlayerMovement.instance.transform.position, levelExit.transform.position) < 2f)
+            {
+                levelExit.transform.position += new Vector3(4f, 0, 0);
+            }
+
+            levelExit.SetActive(true);
+
+            
+        }
+    }
+}
+
+[System.Serializable]
+public class BossAction
+{
+    [Header("Action")]
+    public float actionLength;
+
+    public bool shouldMove;
+    public bool shouldChasePlayer;
+    public float moveSpeed;
+    public bool moveToPoint;
+    public Transform pointToMoveTo;
+
+    public bool shouldShoot;
+    public GameObject itemToShoot;
+    public float timeBwShots;
+    public Transform[] shotPoints;
+
 }
