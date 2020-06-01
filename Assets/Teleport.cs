3d84a27651f24c475778780090bd6e738465198b
Siddhant Rathor <cdsid10@gmail.com>
2020-06-01 13:15:34 +0100

    Wokring on Teleprting enemy, adding explosion rn

--- Assets/Teleport.cs
+++ Assets/Teleport.cs
@@ -9,6 +9,10 @@ public class Teleport : MonoBehaviour
     public Transform player;
     public Animator anim;
 
+    public GameObject explosion;
+
+    private bool canTele;
+    
     private void Awake()
     {
         instance = this;
@@ -28,11 +32,41 @@ public class Teleport : MonoBehaviour
 
     IEnumerator Tele()
     {
+        if (canTele)
+        {
+            anim.SetTrigger("startTele");
+            yield return new WaitForSeconds(2);
+            
+            transform.position = new Vector3(player.position.x + 2, player.position.y, player.position.z);
+            yield return new WaitForSeconds(1);
+            transform.position = gameObject.transform.position;
+            anim.SetTrigger("endTele");
+            
+            anim.SetTrigger("charging");
+
+            yield return new WaitForSeconds(2);
+
+
+            anim.SetTrigger("dead");
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            
+
+
+        }
         
-        transform.position = new Vector3(player.position.x + 2, player.position.y , player.position.z);
-        yield return new WaitForSeconds(3);
-        Destroy(gameObject);
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        canTele = true;
+        StartCoroutine(Tele());
+
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
         
     }
 }
