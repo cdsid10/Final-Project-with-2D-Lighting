96a0f563a58dac687ffad9ee58c07b9910a5272d
Siddhant Rathor <cdsid10@gmail.com>
2020-06-09 14:16:17 +0100

    Making Boss Behavior 3

--- Assets/Boss_Idle.cs
+++ Assets/Boss_Idle.cs
@@ -2,54 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boss_Walk : StateMachineBehaviour
+public class Boss_Idle : StateMachineBehaviour
 {
     public float timer;
     public float minTime;
     public float maxTime;
 
-    public Rigidbody2D theRB;
-    public Vector3 moveDirection;
-    private Transform playerPos;
-    public float speed;
-
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
-        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        theRB = animator.GetComponent<Rigidbody2D>();
         timer = Random.Range(minTime, maxTime);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if (timer <= 0)
+        if(timer <= 0)
         {
-            animator.SetTrigger("idle");
+            animator.SetTrigger("jump");
         }
-        else if(timer > 0)
+        else
         {
-
             timer -= Time.deltaTime;
         }
-
-       // moveDirection = PlayerMovement.instance.transform.position - animator.transform.position;
-
-        //moveDirection.Normalize();
-
-       // theRB.velocity = moveDirection * speed;
-
-        //Vector2 target = new Vector2(playerPos.position.x, playerPos.position.y);
-        //animator.transform.position = Vector2.MoveTowards(animator.transform.position, target, speed);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        
     }
 
-
+    
 }
--- Assets/Scripts/BossBehavior.cs
+++ Assets/Scripts/BossBehavior.cs
@@ -5,19 +5,40 @@ using UnityEngine;
 public class BossBehavior : MonoBehaviour
 {
     public Transform player;
+    public Animator anim;
 
     public bool isFlipped = false;
+
+    private Vector3 newPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = gameObject.GetComponent<Animator>();   
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator Tele()
+    {
+        anim.SetTrigger("jump");
+        yield return new WaitForSeconds(1);
+
+        transform.position = new Vector3(Random.Range(player.position.x - 5, player.position.x + 5), player.position.y, player.position.z);
+        newPos = transform.position;
+
+        yield return new WaitForSeconds(0.5f);
+        anim.SetTrigger("endJump");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(Tele());
+        return;
     }
 
     public void LookAtPlayer()
